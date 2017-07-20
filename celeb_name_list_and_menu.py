068b844a4733c1f250c209f3b5a38184585d18e9
mnaba0103 <mnabavian1@student.gn.k12.ny.us>
2017-07-20 14:55:45 -0400

    Add files via upload

---
+++ celeb_name_list_and_menu.py
@@ -0,0 +1,13 @@
+
+import random
+random_name_list = ["Beyonce", "Kevin Hart", "Ariana Grande", "Michael Phelps", "Bruno Mars", "Ke$ha", "Migos", "Travis Scott", "Phillip Phillips", "Doctor Dre", "Odell Beckham Jr", "Lebron James"]
+print(random_name_list[random.randint(0,11)])
+
+
+import random
+random_appetizer = ["French Fries", "Breadsticks", "Onion Rings", "Morrocan Cigars", "Salad", "Soup"]
+random_entree = ["Pizza", "Burger", "Rice with Meat", "Sandwich", "Mac and Cheese", "Chicken Nuggets"]
+random_dessert = ["Ice Cream", "Cookies", "Rice Pudding", "Cake", "Brownies", "Cupcake"]
+print(random_appetizer[random.randint(0,5)])
+print(random_entree[random.randint(0,5)])
+print(random_dessert[random.randint(0,5)])
